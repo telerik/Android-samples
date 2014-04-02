c51ec8042f499fc2cb48ba33b578108896a33539
kiril.stanoev@telerik.com <kiril.stanoev@telerik.com>
2014-04-02 15:57:29 +0300

    Initial commit

---
+++ Blogs/Seismograph/V2/seismograph/src/main/java/com/telerik/demo/seismograph/SeismicDataPoint.java
@@ -0,0 +1,14 @@
+package com.telerik.demo.seismograph;
+
+public class SeismicDataPoint {
+
+    public int x;
+    public float y;
+
+    public SeismicDataPoint(int x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+
