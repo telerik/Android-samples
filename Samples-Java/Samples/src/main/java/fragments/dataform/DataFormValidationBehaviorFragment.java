2254e4df265c9d3f49f742fd6e1e58cdab63baef
Viktor Skarlatov <viktor.skarlatov@telerik.com>
2015-09-25 18:01:32 +0300

    Working on the data form validation behavior example.

--- Samples-Java/Samples/src/main/java/fragments/dataform/DataFormValidationBehaviorFragment.java
+++ Samples-Java/Samples/src/main/java/fragments/dataform/DataFormValidationBehaviorFragment.java
@@ -1,10 +1,44 @@
 package fragments.dataform;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.telerik.android.sdk.R;
+import com.telerik.widget.dataform.engine.NonEmptyValidator;
+import com.telerik.widget.dataform.visualization.DataFormLinearLayoutManager;
+import com.telerik.widget.dataform.visualization.RadDataForm;
+import com.telerik.widget.dataform.visualization.ValidationAnimationBehavior;
+import com.telerik.widget.dataform.visualization.core.EntityPropertyEditor;
+
 import activities.ExampleFragment;
 
-public class DataFormValidationBehaviorFragment implements ExampleFragment {
+public class DataFormValidationBehaviorFragment extends Fragment implements ExampleFragment {
     @Override
     public String title() {
         return "Validation Behavior";
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootLayout = (ViewGroup)inflater.inflate(R.layout.fragment_dataform_grouping, null);
+
+        RadDataForm dataForm = new RadDataForm(this.getActivity());
+        dataForm.setLayoutManager(new DataFormLinearLayoutManager(this.getActivity());
+        Person person = new Person();
+        person.setName("Joe");
+        dataForm.setEntity(person);
+
+        EntityPropertyEditor nameEditor = (EntityPropertyEditor) dataForm.getExistingEditorForProperty("Name");
+        nameEditor.property().setValidator(new NonEmptyValidator());
+        nameEditor.setValidationViewBehavior(new ValidationAnimationBehavior(this.getActivity()));
+
+        rootLayout.addView(dataForm);
+
+        return rootLayout;
     }
 }
