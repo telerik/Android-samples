89fcc4c3bdc1a7374b11fd9840879b46746a9370
Viktor Skarlatov <viktor.skarlatov@telerik.com>
2015-09-25 18:25:29 +0300

    Implemented the validation behavior SDK example.

--- Samples-Java/Samples/src/main/java/fragments/dataform/DataFormValidationBehaviorFragment.java
+++ Samples-Java/Samples/src/main/java/fragments/dataform/DataFormValidationBehaviorFragment.java
@@ -1,15 +1,19 @@
 package fragments.dataform;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.telerik.android.sdk.R;
 import com.telerik.widget.dataform.engine.NonEmptyValidator;
+import com.telerik.widget.dataform.engine.ValidationInfo;
 import com.telerik.widget.dataform.visualization.DataFormLinearLayoutManager;
+import com.telerik.widget.dataform.visualization.DataFormValidationViewBehavior;
 import com.telerik.widget.dataform.visualization.RadDataForm;
 import com.telerik.widget.dataform.visualization.ValidationAnimationBehavior;
 import com.telerik.widget.dataform.visualization.core.EntityPropertyEditor;
@@ -28,7 +32,7 @@ public class DataFormValidationBehaviorFragment extends Fragment implements Exam
         ViewGroup rootLayout = (ViewGroup)inflater.inflate(R.layout.fragment_dataform_grouping, null);
 
         RadDataForm dataForm = new RadDataForm(this.getActivity());
-        dataForm.setLayoutManager(new DataFormLinearLayoutManager(this.getActivity());
+        dataForm.setLayoutManager(new DataFormLinearLayoutManager(this.getActivity()));
         Person person = new Person();
         person.setName("Joe");
         dataForm.setEntity(person);
@@ -37,8 +41,40 @@ public class DataFormValidationBehaviorFragment extends Fragment implements Exam
         nameEditor.property().setValidator(new NonEmptyValidator());
         nameEditor.setValidationViewBehavior(new ValidationAnimationBehavior(this.getActivity()));
 
+        ((EntityPropertyEditor) dataForm.getExistingEditorForProperty("Mail")).setValidationViewBehavior(new BlinkValidationBehavior(this.getActivity()));
+
         rootLayout.addView(dataForm);
 
         return rootLayout;
+    }
+
+    public class BlinkValidationBehavior extends DataFormValidationViewBehavior {
+        public BlinkValidationBehavior(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void showNegativeFeedback(ValidationInfo info) {
+            super.showNegativeFeedback(info);
+
+            final View editorView = this.editor.rootLayout();
+
+            ViewCompat.animate(editorView).alpha(0).setDuration(50).withEndAction(new Runnable() {
+                @Override
+                public void run() {
+                    ViewCompat.animate(editorView).alpha(1).setDuration(50).withEndAction(new Runnable() {
+                        @Override
+                        public void run() {
+                            ViewCompat.animate(editorView).alpha(0).setDuration(50).withEndAction(new Runnable() {
+                                @Override
+                                public void run() {
+                                    ViewCompat.animate(editorView).alpha(1).setDuration(50);
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        }
     }
 }
