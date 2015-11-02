e86fb7d83c6d0ae96627dab7f4385c6d811d3de0
tgpetrov <Todor.Petrov@Telerik.com>
2015-11-02 18:17:24 +0200

    removed dataform references from xamarin solution

--- Samples-Xamarin/Adapters/ControlsAdapter.cs
+++ Samples-Xamarin/Adapters/ControlsAdapter.cs
@@ -16,7 +16,6 @@ namespace Samples
 			this.source.Add (new CalendarExamples ());
 			this.source.Add (new ListViewExamples ());
 			this.source.Add (new SideDrawerExamples ());
-			this.source.Add (new DataFormExamples ());
 
 		}
 
