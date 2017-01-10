2e48d21f1c08f8c5336f21df5537865ef1f75219
Viktor Skarlatov <viktor.skarlatov@telerik.com>
2017-01-10 19:17:41 +0200

    Added the tab view sdk examples.

--- Samples-Java/Samples/src/main/java/fragments/tabview/TabViewGettingStartedFragment.java
+++ Samples-Java/Samples/src/main/java/fragments/tabview/TabViewGettingStartedFragment.java
@@ -3,15 +3,22 @@ package fragments.tabview;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.telerik.android.common.Util;
+import com.telerik.android.primitives.widget.tabstrip.Tab;
+import com.telerik.android.primitives.widget.tabview.RadTabView;
 import com.telerik.android.sdk.R;
 
 import activities.ExampleFragment;
 
 public class TabViewGettingStartedFragment extends Fragment implements ExampleFragment {
+    RadTabView tabView;
+
     @Override
     public String title() {
         return "Getting Started";
@@ -19,6 +26,18 @@ public class TabViewGettingStartedFragment extends Fragment implements ExampleFr
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_view_getting_started, null);
+
+        tabView = Util.getLayoutPart(view, R.id.tabView, RadTabView.class);
+
+        for(int i = 1; i <= 3; i++) {
+            Tab tab = new Tab("Tab " + Integer.toString(i));
+            TextView contentView = new TextView(this.getContext());
+            contentView.setText("Content view for tab " + Integer.toString(i));
+            contentView.setGravity(Gravity.CENTER);
+            tab.setContentView(contentView);
+
+            tabView.getTabs().add(tab);
+        }
 
         return view;
     }
--- Samples-Java/Samples/src/main/java/fragments/tabview/TabViewTabsPositionFragment.java
+++ Samples-Java/Samples/src/main/java/fragments/tabview/TabViewTabsPositionFragment.java
@@ -3,15 +3,26 @@ package fragments.tabview;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.telerik.android.common.Util;
+import com.telerik.android.primitives.widget.tabstrip.Tab;
+import com.telerik.android.primitives.widget.tabstrip.TabItemView;
+import com.telerik.android.primitives.widget.tabstrip.TabsAlignment;
+import com.telerik.android.primitives.widget.tabview.RadTabView;
+import com.telerik.android.primitives.widget.tabview.TabViewChangeListener;
 import com.telerik.android.sdk.R;
 
 import activities.ExampleFragment;
 
-public class TabViewTabsPositionFragment extends Fragment implements ExampleFragment {
+public class TabViewTabsPositionFragment extends Fragment implements ExampleFragment, TabViewChangeListener, Button.OnClickListener {
+    RadTabView tabView;
+
     @Override
     public String title() {
         return "Tabs Position";
@@ -19,7 +30,133 @@ public class TabViewTabsPositionFragment extends Fragment implements ExampleFrag
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_view_position_fragment, null);
+        tabView = Util.getLayoutPart(view, R.id.tabView, RadTabView.class);
+        tabView.addChangeListener(this);
+
+        tabView.getTabs().add(new Tab("Tab 1"));
+        tabView.getTabs().add(new Tab("Tab 2"));
+        tabView.getTabs().add(new Tab("Tab 3"));
+        tabView.getTabs().add(new Tab("Tab 4"));
+        tabView.getTabs().add(new Tab("Tab 5"));
+        tabView.getTabs().add(new Tab("Tab 6"));
+        tabView.getTabs().add(new Tab("Tab 7"));
+        tabView.getTabs().add(new Tab("Tab 8"));
+
+        int[] ids = new int[] { R.id.leftPosition, R.id.rightPosition, R.id.topPosition, R.id.bottomPosition,
+                R.id.leftAlignment, R.id.rightAlignment, R.id.topAlignment, R.id.bottomAlignment, R.id.centerAlignment, R.id.stretchAlignment };
+
+        for (int id : ids) {
+            Button btn = Util.getLayoutPart(view, id, Button.class);
+            btn.setOnClickListener(this);
+        }
 
         return view;
+    }
+
+    public void onTopPosition(View view) {
+        this.tabView.setTabsPosition(com.telerik.android.primitives.widget.tabview.TabsPosition.TOP);
+    }
+
+    public void onBottomPosition(View view) {
+        this.tabView.setTabsPosition(com.telerik.android.primitives.widget.tabview.TabsPosition.BOTTOM);
+    }
+
+    public void onLeftPosition(View view) {
+        this.tabView.setTabsPosition(com.telerik.android.primitives.widget.tabview.TabsPosition.LEFT);
+    }
+
+    public void onRightPosition(View view) {
+        this.tabView.setTabsPosition(com.telerik.android.primitives.widget.tabview.TabsPosition.RIGHT);
+    }
+
+    public void onLeftAlignment(View view) {
+        this.tabView.getTabStrip().setTabsAlignment(TabsAlignment.LEFT);
+    }
+
+    public void onRightAlignment(View view) {
+        this.tabView.getTabStrip().setTabsAlignment(TabsAlignment.RIGHT);
+    }
+
+    public void onTopAlignment(View view) {
+        this.tabView.getTabStrip().setTabsAlignment(TabsAlignment.TOP);
+    }
+
+    public void onBottomAlignment(View view) {
+        this.tabView.getTabStrip().setTabsAlignment(TabsAlignment.BOTTOM);
+    }
+
+    public void onCenterAlignment(View view) {
+        this.tabView.getTabStrip().setTabsAlignment(TabsAlignment.CENTER);
+    }
+
+    public void onStretchAlignment(View view) {
+        this.tabView.getTabStrip().setTabsAlignment(TabsAlignment.STRETCH);
+    }
+
+    @Override
+    public View getContentViewForTab(Tab tab) {
+        TextView contentView = new TextView(this.getContext());
+        contentView.setText(tab.getTitle() + " content view.");
+        contentView.setGravity(Gravity.CENTER);
+
+        return contentView;
+    }
+
+    @Override
+    public TabItemView getViewForTab(Tab tab) {
+        return null;
+    }
+
+    @Override
+    public boolean onSelectingTab(Tab tabToSelect) {
+        return false;
+    }
+
+    @Override
+    public void onTabSelected(Tab selectedTab, Tab deselectedTab) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.topPosition) {
+            this.onTopPosition(v);
+        }
+
+        if(v.getId() == R.id.leftPosition) {
+this.onLeftPosition(v);
+        }
+
+        if(v.getId() == R.id.rightPosition) {
+this.onRightPosition(v);
+        }
+
+        if(v.getId() == R.id.bottomPosition) {
+this.onBottomPosition(v);
+        }
+
+        if(v.getId() == R.id.topAlignment) {
+this.onTopAlignment(v);
+        }
+
+        if(v.getId() == R.id.leftAlignment) {
+this.onLeftAlignment(v);
+        }
+
+        if(v.getId() == R.id.bottomAlignment) {
+this.onBottomAlignment(v);
+        }
+
+        if(v.getId() == R.id.rightAlignment) {
+this.onRightAlignment(v);
+        }
+
+        if(v.getId() == R.id.centerAlignment) {
+this.onCenterAlignment(v);
+        }
+
+        if(v.getId() == R.id.stretchAlignment) {
+this.onStretchAlignment(v);
+        }
     }
 }
