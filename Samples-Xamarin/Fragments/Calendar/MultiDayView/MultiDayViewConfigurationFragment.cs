d79ac9ac6da076c1af8e6d555061e7019bcc910a
ni4ka7a <n.atanasow@yahoo.com>
2018-08-02 14:27:11 +0300

    Add MultiDayView getting started and configuration examples

--- Samples-Xamarin/Fragments/Calendar/MultiDayView/MultiDayViewConfigurationFragment.cs
+++ Samples-Xamarin/Fragments/Calendar/MultiDayView/MultiDayViewConfigurationFragment.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using Android.Content;
 using Android.OS;
 using Android.Views;
 using Android.Widget;
 using Com.Telerik.Android.Primitives.Widget.Sidedrawer;
 using Com.Telerik.Widget.Calendar;
 using Com.Telerik.Widget.Calendar.Events;
+using Java.Text;
 using Java.Util;
 
 namespace Samples
@@ -29,6 +31,9 @@ namespace Samples
 
             var events = GenerateClendarEvents();
             this.calendarView.EventAdapter.Events = events;
+
+            this.calendarView.MultiDayView.EventViewTapListener = new EventViewTapListener(Context);
+            this.calendarView.MultiDayView.AddTimeSlotTappedListener(new TimeSlotTappedListener(Context));
 
             RadSideDrawer drawer = new RadSideDrawer(Activity);
 
@@ -83,6 +88,7 @@ namespace Samples
             spinnerArray.Add(2);
             spinnerArray.Add(3);
             spinnerArray.Add(4);
+            spinnerArray.Add(5);
             spinnerArray.Add(6);
             spinnerArray.Add(7);
 
@@ -90,7 +96,7 @@ namespace Samples
             ArrayAdapter<int> spinnerArrayAdapter = new ArrayAdapter<int>(Activity, Android.Resource.Layout.SimpleSpinnerDropDownItem, spinnerArray);
             spinner.Adapter = spinnerArrayAdapter;
 
-            spinner.SetSelection(2);
+            spinner.SetSelection(4);
             spinner.ItemSelected += (object sender, AdapterView.ItemSelectedEventArgs e) => 
             {
                 this.calendarView.MultiDayView.VisibleDays = spinnerArray[e.Position];
@@ -100,6 +106,45 @@ namespace Samples
             container.AddView(spinner);
 
             return container;
+        }
+
+        public class EventViewTapListener : Java.Lang.Object, Com.Telerik.Widget.Calendar.Dayview.CalendarDayView.IEventViewTapListener
+        {
+            private Context context;
+
+            public EventViewTapListener(Context context)
+            {
+                this.context = context;
+            }
+
+            public void OnEventViewTap(Event p0)
+            {
+                Toast.MakeText(this.context, String.Format("Еvent clicked: {0}", p0.Title), ToastLength.Short).Show();
+            }
+        }
+
+        public class TimeSlotTappedListener : Java.Lang.Object, Com.Telerik.Widget.Calendar.Dayview.IDayViewTimeSlotTappedListener
+        {
+            private Context context;
+            private SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
+
+            public TimeSlotTappedListener(Context context)
+            {
+                this.context = context;
+            }
+
+            public void OnTimeSlotTapped(long exactTime, long startTime, long endTime)
+            {
+                Calendar calendar = Calendar.Instance;
+                calendar.TimeInMillis = startTime;
+
+                string startTimeString = dateFormat.Format(calendar.Time);
+
+                calendar.TimeInMillis = endTime;
+                string endTimeString = dateFormat.Format(calendar.Time);
+
+                Toast.MakeText(this.context, String.Format("TimeSlot clicked: {0} - {1}", startTimeString, endTimeString), ToastLength.Short).Show();
+            }
         }
     }
 }
